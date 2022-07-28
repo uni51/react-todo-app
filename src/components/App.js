2a450e506e67964f6909048f3f12e7bfc1642dc6
Koichi.Shimomura <unieye51@gmail.com>
2022-07-28 17:27:49 +0900

    6-34 Todoアプリ 実装01

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,38 @@
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-export default function App() {
-  const fetchData = async () => {
-    const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/users"
-    );
+const todoDataUrl = "http://localhost:3100/todos";
 
-    return console.log(response.data);
-  };
+function App() {
+  const [todoList, setTodoList] = useState([]);
 
-  fetchData();
+  // useEffect()を利用することで、コンポーネントのマウント後に処理を実行
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(todoDataUrl);
 
-  return <h1>Hello React!!</h1>;
+      setTodoList(response.data);
+    };
+    fetchData();
+  }, []);
+
+  console.log("TODOリスト:", todoList);
+
+  return (
+    <>
+      <h1>TODO進捗管理</h1>
+      <textarea />
+      <button>+ TODOを追加</button>
+      <h2>TODOリスト</h2>
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>
+            {todo.content}({todo.done ? "完了" : "未完了"})
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
+
+export default App;
