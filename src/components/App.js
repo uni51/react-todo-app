a14139f46bab966d24a5f2e35f31f4b5a01da370
Koichi.Shimomura <unieye51@gmail.com>
2022-07-30 11:23:32 +0900

    6-34 Todoアプリ 実装03 App.js内でのコンポーネント化

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,34 @@ import axios from "axios";
 // ローカルに準備したモックサーバのURL
 const todoDataUrl = "http://localhost:3100/todos";
 
+const TodoTitle = ({ title, as }) => {
+  if (as === "h1") return <h1>{title}</h1>;
+
+  if (as === "h2") return <h2>{title}</h2>;
+
+  return <p>{title}</p>;
+};
+
+const TodoItem = ({ todo }) => {
+  return (
+    <li>
+      {todo.content}
+      <button>{todo.done ? "未完了リストへ" : "完了リストへ"}</button>
+      <button>削除</button>
+    </li>
+  );
+};
+
+const TodoList = ({ todoList }) => {
+  return (
+    <ul>
+      {todoList.map((todo) => (
+        <TodoItem todo={todo} key={todo.id} />
+      ))}
+    </ul>
+  );
+};
+
 function App() {
   const [todoList, setTodoList] = useState([]);
 
@@ -23,40 +51,20 @@ function App() {
     return !todo.done;
   });
 
-  console.log("未完了TODOリスト:", inCompletedList);
-
   const completedList = todoList.filter((todo) => {
     return todo.done;
   });
 
-  console.log("完了TODOリスト:", completedList);
-
   return (
     <>
-      <h1>TODO進捗管理</h1>
+      <TodoTitle title="TODO進捗管理" as="h1" />
       <textarea />
       <button>+ TODOを追加</button>
-      <h2>未完了TODOリスト</h2>
-      <ul>
-        {inCompletedList.map((todo) => (
-          <li key={todo.id}>
-            {todo.content}
-            <button>{todo.done ? "未完了リストへ" : "完了リストへ"}</button>
-            <button>削除</button>
-          </li>
-        ))}
-      </ul>
+      <TodoTitle title="未完了TODOリスト" as="h2" />
+      <TodoList todoList={inCompletedList} />
 
-      <h2>完了TODOリスト</h2>
-      <ul>
-        {completedList.map((todo) => (
-          <li key={todo.id}>
-            {todo.content}
-            <button>{todo.done ? "未完了リストへ" : "完了リストへ"}</button>
-            <button>削除</button>
-          </li>
-        ))}
-      </ul>
+      <TodoTitle title="完了TODOリスト" as="h2" />
+      <TodoList todoList={completedList} />
     </>
   );
 }
