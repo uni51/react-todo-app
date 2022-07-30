09cf3bea96b2c38ec624c68b2655470aec487ca2
Koichi.Shimomura <unieye51@gmail.com>
2022-07-30 11:09:45 +0900

    6-34 Todoアプリ 実装02

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
+// ローカルに準備したモックサーバのURL
 const todoDataUrl = "http://localhost:3100/todos";
 
 function App() {
@@ -18,16 +19,41 @@ function App() {
 
   console.log("TODOリスト:", todoList);
 
+  const inCompletedList = todoList.filter((todo) => {
+    return !todo.done;
+  });
+
+  console.log("未完了TODOリスト:", inCompletedList);
+
+  const completedList = todoList.filter((todo) => {
+    return todo.done;
+  });
+
+  console.log("完了TODOリスト:", completedList);
+
   return (
     <>
       <h1>TODO進捗管理</h1>
       <textarea />
       <button>+ TODOを追加</button>
-      <h2>TODOリスト</h2>
+      <h2>未完了TODOリスト</h2>
       <ul>
-        {todoList.map((todo) => (
+        {inCompletedList.map((todo) => (
           <li key={todo.id}>
-            {todo.content}({todo.done ? "完了" : "未完了"})
+            {todo.content}
+            <button>{todo.done ? "未完了リストへ" : "完了リストへ"}</button>
+            <button>削除</button>
+          </li>
+        ))}
+      </ul>
+
+      <h2>完了TODOリスト</h2>
+      <ul>
+        {completedList.map((todo) => (
+          <li key={todo.id}>
+            {todo.content}
+            <button>{todo.done ? "未完了リストへ" : "完了リストへ"}</button>
+            <button>削除</button>
           </li>
         ))}
       </ul>
